1a6da62b50e224b5617913d86c239d63ce3b65f7
Emrick19 <emricky17@gmail.com>
2020-10-05 16:46:57 +0100

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div style={{ color: `teal` }}>
+      <h1>About Gatsby</h1>
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
-import React from "react"
+import React from 'react'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <p>I'm Ademola, a fullstack developer living in beautiful Nigeria.</p>
+    </div>
+  )
 }
+
+export default HomePage
