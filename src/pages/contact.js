4e14a78c688832e1b383ab9a7f37e4f197ff64fa
Emrick19 <emricky17@gmail.com>
2020-10-05 16:59:59 +0100

    Added a paragraph to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,6 +4,7 @@ const ContactPage = () => {
     return (
         <div>
             <h1>Contact Me.</h1>
+            <p>The bes way to react me is via @emrricky19 on twitter</p>
         </div>
     )
 }
