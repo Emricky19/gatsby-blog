7047e3f0de906c6c7fe834170515fe9a41556578
Emrick19 <emricky17@gmail.com>
2020-10-12 22:16:53 +0100

    configured it to load up .env file

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,15 @@ module.exports = {
     title: `FUll-Stack Bootcamp`,
     author: 'Ademola Richard'
   },
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [`gatsby-plugin-sass`,
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: `src`,
+      path: `${__dirname}/src/`,
+      ignore: [`**/\.*`], // ignore files starting with a dot
+    },
+  },
+  'gatsby-transformer-remark'
+],
 }
